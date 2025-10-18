fdbf26590a78f040b6cab0e8d62006ea2acda398
Vladyslav Zasoba <contact@ownerofglory.com>
2025-10-18 18:31:16 +0200

    onboard: [feat] draft onboard orchestrator

---
+++ internal/orchestrator/onboard.go
@@ -0,0 +1,132 @@
+package orchestrator
+
+import (
+	"context"
+	"fmt"
+	"log/slog"
+	"os"
+	"time"
+
+	"github.com/ownerofglory/raspi-agent/internal/core/domain"
+	"github.com/ownerofglory/raspi-agent/internal/core/ports"
+)
+
+type onboardOrchestrator struct {
+	listener ports.WakeListener
+	recorder ports.Recorder
+}
+
+func NewOnboardOrchestrator(listener ports.WakeListener, recorder ports.Recorder) *onboardOrchestrator {
+	return &onboardOrchestrator{
+		listener: listener,
+		recorder: recorder,
+	}
+}
+
+func (o *onboardOrchestrator) Run(ctx context.Context) error {
+	ctxWithCancel, cancel := context.WithCancel(ctx)
+
+	recordCh := make(chan domain.RecordingResult)
+	defer close(recordCh)
+
+	// wake-record routine
+	go func() {
+		err := o.recordUponWake(ctxWithCancel, recordCh)
+		if err != nil {
+			cancel()
+			return
+		}
+	}()
+
+	fpCh := make(chan string)
+	defer close(fpCh)
+
+	// record processing routine
+	go func() {
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case recordResult, ok := <-recordCh:
+				if !ok {
+					return
+				}
+				filePath := "recording" + time.Now().Format(time.RFC3339) + ".wav"
+				func() {
+					f, err := os.Create(filePath)
+					if err != nil {
+						slog.Error("Failed to create recording file", "err", err)
+						cancel()
+						return
+					}
+					defer f.Close()
+
+					err = recordResult.SaveTo(f)
+					if err != nil {
+						slog.Error("Unable to save recording", "error", err)
+						cancel()
+						return
+					}
+				}()
+				fpCh <- filePath
+			}
+		}
+	}()
+
+	// request backend
+	go func() {
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case filePath, ok := <-fpCh:
+				if !ok {
+					return
+				}
+
+				_ = filePath
+			}
+		}
+	}()
+
+	<-ctxWithCancel.Done()
+	return nil
+}
+
+func (o *onboardOrchestrator) recordUponWake(ctx context.Context, resCh chan<- domain.RecordingResult) error {
+	wakeCh := make(chan error)
+	defer close(wakeCh)
+
+	for {
+		select {
+		case <-ctx.Done():
+			return fmt.Errorf("context canceled: %w", ctx.Err())
+		default:
+			// wake listener routine
+			go func() {
+				err := o.listener.Listen(ctx)
+				if err != nil {
+					slog.Error("Failed to listen for a wake word", "error", err)
+					wakeCh <- fmt.Errorf("failed to listen for a wake word: %w", err)
+					return
+				}
+
+				wakeCh <- nil
+			}()
+
+			err := <-wakeCh
+			if err != nil {
+				slog.Error("Failed to send a wake word", "error", err)
+				return fmt.Errorf("failed to send a wake word: %w", err)
+			}
+
+			audio, err := o.recorder.RecordAudio(ctx, 8*time.Second)
+			if err != nil {
+				slog.Error("Failed to record audio input", "error", err)
+				return fmt.Errorf("failed to record audio input: %w", err)
+			}
+
+			resCh <- audio
+		}
+	}
+}
