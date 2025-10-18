0aaf720a5e381025256283b4dd333b8a05ca7107
Vladyslav Zasoba <contact@ownerofglory.com>
2025-10-18 17:54:50 +0200

    backend: [feat] Porcupine demo

--- cmd/raspi-agent-onboard/main.go
+++ cmd/raspi-agent-onboard/main.go
@@ -1,3 +1,47 @@
 package main
 
-func main() {}
+import (
+	"context"
+	"flag"
+	"log/slog"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/ownerofglory/raspi-agent/internal/wakeword"
+)
+
+var (
+	porcupineAccessKey   = flag.String("porcupineAccessKey", "", "porcupine SDK access key from 'https://console.picovoice.ai/'")
+	porcupineLibPath     = flag.String("porcupineLibPath", "", "porcupine library path e.g. 'lib/libpv_porcupine.so'")
+	porcupineModelPath   = flag.String("porcupineModelPath", "", "porcupine model parameters path, e.g. 'resources/porcupine_params.pv'")
+	porcupineKeywordPath = flag.String("porcupineKeywordPath", "", "porcupine keyword path, e.g. 'resources/Hey-Rhaspy_en_raspberry-pi_v3_0_0.ppn'")
+)
+
+func main() {
+	flag.Parse()
+
+	slog.SetLogLoggerLevel(slog.LevelDebug)
+	slog.Debug("Starting application")
+
+	listener := wakeword.NewPorcupineListener(*porcupineAccessKey, *porcupineModelPath, *porcupineLibPath, *porcupineKeywordPath)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	go func() {
+		err := listener.Listen(ctx)
+		if err != nil {
+			slog.Error("Failed to listen", "err", err)
+			return
+		}
+
+		slog.Debug("Wakeword detected")
+	}()
+
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
+	<-sigCh
+
+	slog.Debug("Received signal, shutting down")
+}
