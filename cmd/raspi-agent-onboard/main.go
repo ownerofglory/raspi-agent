27e1246e1de64359b8c3c0c1110ffa6916cf2f7c
Vladyslav Zasoba <contact@ownerofglory.com>
2025-10-19 15:35:47 +0200

    onboard: [feat] integrate orchestrator

--- cmd/raspi-agent-onboard/main.go
+++ cmd/raspi-agent-onboard/main.go
@@ -8,6 +8,9 @@ import (
 	"os/signal"
 	"syscall"
 
+	"github.com/ownerofglory/raspi-agent/internal/audio"
+	"github.com/ownerofglory/raspi-agent/internal/http/v1/client"
+	"github.com/ownerofglory/raspi-agent/internal/orchestrator"
 	"github.com/ownerofglory/raspi-agent/internal/wakeword"
 )
 
@@ -16,6 +19,8 @@ var (
 	porcupineLibPath     = flag.String("porcupineLibPath", "", "porcupine library path e.g. 'lib/libpv_porcupine.so'")
 	porcupineModelPath   = flag.String("porcupineModelPath", "", "porcupine model parameters path, e.g. 'resources/porcupine_params.pv'")
 	porcupineKeywordPath = flag.String("porcupineKeywordPath", "", "porcupine keyword path, e.g. 'resources/Hey-Rhaspy_en_raspberry-pi_v3_0_0.ppn'")
+
+	backendBaseURL = flag.String("backendBaseURL", "", "Backend base URL")
 )
 
 func main() {
@@ -25,18 +30,20 @@ func main() {
 	slog.Debug("Starting application")
 
 	listener := wakeword.NewPorcupineListener(*porcupineAccessKey, *porcupineModelPath, *porcupineLibPath, *porcupineKeywordPath)
+	recorder := audio.NewRecorder()
+	player := audio.NewPortAudioPlayer()
+	assistant := client.NewVoiceAssistant(*backendBaseURL)
 
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
+	orch := orchestrator.NewOnboardOrchestrator(listener, recorder, player, assistant)
 	go func() {
-		err := listener.Listen(ctx)
+		err := orch.Run(ctx)
 		if err != nil {
-			slog.Error("Failed to listen", "err", err)
+			slog.Error("Error running onboardorchestrator", "error", err)
 			return
 		}
-
-		slog.Debug("Wakeword detected")
 	}()
 
 	sigCh := make(chan os.Signal, 1)
